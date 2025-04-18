9c0e05091328e13e185f0f5e7d36bbfea3172e04
Bectaly13 <dodocad13@gmail.com>
2025-04-19 01:15:54 +0200

    Back button

--- frontend/src/app/study/study.page.ts
+++ frontend/src/app/study/study.page.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, ViewWillEnter } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, ViewWillEnter, IonButtons, IonBackButton } from '@ionic/angular/standalone';
 import { ActivatedRoute } from '@angular/router';
 
 import { MessageService } from '../services/message.service';
@@ -12,7 +12,7 @@ import { ErrorService } from '../services/error.service';
   templateUrl: './study.page.html',
   styleUrls: ['./study.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonBackButton]
 })
 export class StudyPage implements ViewWillEnter {
   sectorId!: number;
