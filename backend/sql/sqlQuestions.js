2828ab2fbe6a71ed3fca3485f1d40dbfe9b1250c
Bectaly13 <dodocad13@gmail.com>
2025-04-19 01:00:44 +0200

    valid question IDs

--- backend/sql/sqlQuestions.js
+++ backend/sql/sqlQuestions.js
@@ -5,16 +5,34 @@ const qgr = require("./sqlConfig").qgr;
 const qsl = require("./sqlConfig").qsl;
 
 async function getValidStudyIds(chapterId, sectorId) {
-    const query = `SELECT DISTINCT ${qst}.questionId
-        FROM ${qst}
+    let validStudyIds = [];
+
+    let query = `SELECT DISTINCT ${qgr}.questionGroupId
+        FROM ${qgr}
+        JOIN ${qst} ON ${qgr}.questionGroupId = ${qst}.questionGroupId
         JOIN ${qsl} ON ${qst}.questionId = ${qsl}.questionId
-        JOIN ${qgr} ON ${qst}.questionGroupId = ${qst}.questionGroupId
         JOIN ${skl} ON ${qgr}.skillId = ${skl}.skillId    
         WHERE ${skl}.chapterId = ?
         AND ${qsl}.sectorId = ?`;
     
-    const data = [chapterId, sectorId];
-    return await mysqlConnect.query(query, data);
+    let data = [chapterId, sectorId];
+    let validQuestionGroupIds =  await mysqlConnect.query(query, data);
+
+    for(let i = 0; i<validQuestionGroupIds.length; i++) {
+        query = `SELECT questionId, questionGroupId, explanation, level, mixingType
+            FROM ${qst}
+            WHERE questionGroupId = ?
+            ORDER BY level`;
+        
+        data = [validQuestionGroupIds[i]["questionGroupId"]];
+        let result = await mysqlConnect.query(query, data);
+
+        for(let j = 0; j<result.length; j++) {
+            validStudyIds.push(result[j]);
+        }
+    }
+
+    return validStudyIds;
 }
 
 module.exports.getValidStudyIds = getValidStudyIds;
