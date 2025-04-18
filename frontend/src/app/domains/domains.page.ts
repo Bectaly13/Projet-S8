d088458777e1616e8194622a673d8382cd0c0113
Bectaly13 <dodocad13@gmail.com>
2025-04-18 14:25:04 +0200

    Domains

--- frontend/src/app/domains/domains.page.ts
+++ frontend/src/app/domains/domains.page.ts
@@ -1,26 +1,34 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, ViewWillEnter, IonButtons, IonBackButton } from '@ionic/angular/standalone';
-import { ActivatedRoute } from '@angular/router';
+import { IonContent, IonHeader, IonTitle, IonToolbar, ViewWillEnter, IonButtons, IonBackButton, IonList, IonItem, IonButton } from '@ionic/angular/standalone';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { MessageService } from '../services/message.service';
 import { ErrorService } from '../services/error.service';
+
+export interface Domain {
+  domainId: number;
+  name: string;
+}
 
 @Component({
   selector: 'app-domains',
   templateUrl: './domains.page.html',
   styleUrls: ['./domains.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonBackButton]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonBackButton, IonList, IonItem, IonButton]
 })
 export class DomainsPage implements ViewWillEnter {
   sectorId!: number;
   sector!: string;
 
+  domains!: Domain[];
+
   constructor(private route: ActivatedRoute,
               private message: MessageService,
-              private error: ErrorService) { }
+              private error: ErrorService,
+              private router: Router) { }
 
   ionViewWillEnter() {
     this.sectorId = Number(this.route.snapshot.queryParamMap.get("sectorId"));
@@ -33,7 +41,28 @@ export class DomainsPage implements ViewWillEnter {
       else {
         this.error.errorMessage(res);
       }
+    });
+
+    this.message.sendMessage("getDomains", {sectorId : this.sectorId}).subscribe(res => {
+      console.log(res);
+      if(res.status == 200) {
+        this.domains = res.data.map(
+          (item: {domainId: number, name: string}) => ({
+            domainId: item.domainId,
+            name: item.name
+          })
+        );
+      }
+      else {
+        this.error.errorMessage(res);
+      }
     })
   }
 
+  goToChapters(index: number) {
+    this.router.navigate(["chapters"], {queryParams: {
+      domainId: index,
+      sectorId: this.sectorId
+    }});
+  }
 }
