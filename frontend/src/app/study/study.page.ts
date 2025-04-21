dfdea17efa3620df06f298e6e0d0b0fb0dbdd51d
Bectaly13 <dodocad13@gmail.com>
2025-04-21 21:56:19 +0200

    Random choice order

--- frontend/src/app/study/study.page.ts
+++ frontend/src/app/study/study.page.ts
@@ -55,6 +55,43 @@ export class StudyPage implements ViewWillEnter {
             console.log(res);
             if(res.status == 200) {
               this.choices[i] = res.data;
+
+              const mixingType = this.questions[i].mixingType;
+
+              if (mixingType === "RANDOM") {
+                const fieldsToShuffle = this.choices[i].map((c: any) => ({
+                  choiceText: c.choiceText,
+                  isCorrect: c.isCorrect
+                }));
+                const shuffled: any[] = this.shuffle(fieldsToShuffle);
+                for (let j = 0; j < this.choices[i].length; j++) {
+                  this.choices[i][j].choiceText = shuffled[j].choiceText;
+                  this.choices[i][j].isCorrect = shuffled[j].isCorrect;
+                }
+              }
+
+              else if (mixingType === "TWO_BY_TWO") {
+                const group1 = this.choices[i].slice(0, 2).map((c: any) => ({
+                  choiceText: c.choiceText,
+                  isCorrect: c.isCorrect
+                }));
+                const group2 = this.choices[i].length > 2 ? this.choices[i].slice(2, 4).map((c: any) => ({
+                  choiceText: c.choiceText,
+                  isCorrect: c.isCorrect
+                })) : [];
+
+                const shuffled1: any[] = this.shuffle(group1);
+                const shuffled2: any[] = this.shuffle(group2);
+
+                for (let j = 0; j < shuffled1.length; j++) {
+                  this.choices[i][j].choiceText = shuffled1[j].choiceText;
+                  this.choices[i][j].isCorrect = shuffled1[j].isCorrect;
+                }
+                for (let j = 0; j < shuffled2.length; j++) {
+                  this.choices[i][j + 2].choiceText = shuffled2[j].choiceText;
+                  this.choices[i][j + 2].isCorrect = shuffled2[j].isCorrect;
+                }
+              }
             }
             else {
               this.error.errorMessage(res);
@@ -79,4 +116,13 @@ export class StudyPage implements ViewWillEnter {
       }
     })
   }
+
+  shuffle<T>(array: T[]): T[] {
+    const shuffled = [...array];
+    for (let i = shuffled.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
+    return shuffled;
+  } 
 }
