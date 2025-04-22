854234d92519f8a8dfcb0b0f356fbc7d7818ced2
Val <valentine.goman@gmail.com>
2025-04-22 12:06:58 +0200

    Component fade created - yet to be coded

---
+++ frontend/src/app/fade/fade.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fade',
+  templateUrl: './fade.component.html',
+  styleUrls: ['./fade.component.scss'],
+})
+export class FadeComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
