73071a511b831592fcfa0997bcf7a41d9a895b8e
Bectaly13 <dodocad13@gmail.com>
2025-04-27 00:24:10 +0200

    relevant skill OPTI

--- frontend/src/app/skills/skills.page.ts
+++ frontend/src/app/skills/skills.page.ts
@@ -11,7 +11,6 @@ import { SharedVariablesService } from '../services/shared-variables.service';
 export interface Skill {
   skillId: number;
   name: string;
-  isRelevant: boolean
 }
 
 @Component({
@@ -52,16 +51,10 @@ export class SkillsPage implements ViewWillEnter {
 
     this.domainsImageName = this.domainsImageUrl + "domains" + this.domainId + ".jpg";
 
-    this.message.sendMessage("getSkills", {chapterId: this.chapterId}).subscribe(res => {
+    this.message.sendMessage("getSkills", {chapterId: this.chapterId, sectorId: this.sectorId}).subscribe(res => {
       console.log(res);
       if(res.status == 200) {
         this.skills = res.data;
-
-        for(let skill of this.skills) {
-          this.message.sendMessage("isSkillRelevant", {skillId: skill.skillId, sectorId: this.sectorId}).subscribe(res => {
-            skill.isRelevant = res.data;
-          })
-        }
       }
       else {
         this.error.errorMessage(res);
