9f286056af38cd7d68720ceee9a56ada4c3c5c3f
Val <valentine.goman@gmail.com>
2025-04-27 11:53:47 +0200

    SQL GetDomains simplifié pour debug

--- backend/sql/sqlDomains.js
+++ backend/sql/sqlDomains.js
@@ -2,28 +2,25 @@ const mysqlConnect = require("./sqlConnect");
 const {dom, chp, skl, qgr, qst, qsl} = require("./sqlConfig")
 
 async function getDomains(sectorId, mcqSize) {
-    const query = `SELECT dom.domainId, dom.name
+    const query = `
+        SELECT dom.domainId, dom.name
         FROM ${dom} AS dom
         WHERE dom.subjectId = 1
         AND EXISTS (
             SELECT 1
-            FROM (
-                SELECT chp.chapterId, qgr.questionGroupId
-                FROM ${chp} AS chp
-                JOIN ${skl} AS skl ON chp.chapterId = skl.chapterId
-                JOIN ${qgr} AS qgr ON skl.skillId = qgr.skillId
-                JOIN ${qst} AS qst ON qgr.questionGroupId = qst.questionGroupId
-                JOIN ${qsl} AS qsl ON qst.questionId = qsl.questionId
-                WHERE chp.domainId = dom.domainId
-                AND qsl.sectorId = ?
-                AND qst.validated = 1
-                GROUP BY chp.chapterId, qgr.questionGroupId
-                HAVING COUNT(qst.questionId) > 0
-            ) AS valid_groups
-            GROUP BY chapterId
-            HAVING COUNT(questionGroupId) >= ?
+            FROM ${chp} AS chp
+            JOIN ${skl} AS skl ON chp.chapterId = skl.chapterId
+            JOIN ${qgr} AS qgr ON skl.skillId = qgr.skillId
+            JOIN ${qst} AS qst ON qgr.questionGroupId = qst.questionGroupId
+            JOIN ${qsl} AS qsl ON qst.questionId = qsl.questionId
+            WHERE chp.domainId = dom.domainId
+            AND qsl.sectorId = ?
+            AND qst.validated = 1
+            GROUP BY chp.chapterId
+            HAVING COUNT(DISTINCT qgr.questionGroupId) >= ?
         )
-        ORDER BY dom.domainId`;
+        ORDER BY dom.domainId;
+        `;
 
     const data = [sectorId, mcqSize];
     return mysqlConnect.query(query, data);
