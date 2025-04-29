2bdb439f028a75069470de38d0d98f9ab5aee78c
Bectaly13 <dodocad13@gmail.com>
2025-04-29 19:07:54 +0200

    stats

--- frontend/src/app/stats/stats.page.ts
+++ frontend/src/app/stats/stats.page.ts
@@ -2,10 +2,27 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonFooter, ViewWillEnter } from '@ionic/angular/standalone';
+import { ActivatedRoute } from '@angular/router';
+
+import { DarkModeService } from '../services/dark-mode.service';
+import { MessageService } from '../services/message.service';
+import { ErrorService } from '../services/error.service';
+import { SharedVariablesService } from '../services/shared-variables.service';
+import { StorageService } from '../services/storage.service';
 
 import { HeaderComponent } from '../header/header.component';
 import { NavbarComponent } from '../navbar/navbar.component';
-import { DarkModeService } from '../services/dark-mode.service';
+
+export interface Domain {
+  domainId: number;
+  name: string;
+}
+
+export interface DomainStats {
+  correct: number;
+  incorrect: number;
+  total: number;
+}
 
 @Component({
   selector: 'app-stats',
@@ -15,10 +32,62 @@ import { DarkModeService } from '../services/dark-mode.service';
   imports: [HeaderComponent, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, NavbarComponent, IonFooter]
 })
 export class StatsPage implements ViewWillEnter{
+  domains: Domain[] = [];
+  domainStats: DomainStats[] = [];
 
-  constructor(private darkmode: DarkModeService) { }
+  sectorId!: number;
+  sector!: string;
+  mcqSize!: number;
 
-  ionViewWillEnter() {
+  constructor(private darkmode: DarkModeService,
+              private message: MessageService,
+              private error: ErrorService,
+              private variables: SharedVariablesService,
+              private route: ActivatedRoute,
+              private storage: StorageService) { }
+
+  async ionViewWillEnter() {
     this.darkmode.init();
+
+    this.sectorId = Number(this.route.snapshot.queryParamMap.get("sectorId"));
+    this.sector = String(this.route.snapshot.queryParamMap.get("sector"));
+
+    this.mcqSize = this.variables.mcqSize.large;
+
+    const sector_data = (await this.storage.get("questions_data"))[this.sectorId];
+
+    this.message.sendMessage("getDomains", {sectorId: this.sectorId, mcqSize: this.mcqSize}).subscribe(res => {
+      console.log(res);
+      if(res.status == 200) {
+        this.domains = res.data;
+
+        for(let domain of this.domains) {
+          let correct: number = 0;
+          let incorrect: number = 0;
+          let total: number = 0;
+
+          const domainId = domain["domainId"];
+          const domain_data = sector_data[domainId];
+
+          for(let chapterId of Object.keys(domain_data)) {
+            const chapter = domain_data[chapterId];
+            correct += chapter.correct.length;
+            incorrect += chapter.incorrect.length;
+            total += chapter.total;
+          }
+
+          const stats: DomainStats = {
+            correct: correct,
+            incorrect: incorrect,
+            total: total
+          };
+
+          this.domainStats.push(stats);
+        }
+      }
+      else {
+        this.error.errorMessage(res);
+      }
+    })
   }
 }
