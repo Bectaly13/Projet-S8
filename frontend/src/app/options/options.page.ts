1c21beeadf8858008cde90de6f70986aef5b5019
Bectaly13 <dodocad13@gmail.com>
2025-05-01 03:23:21 +0200

    clear progress buttons

--- frontend/src/app/options/options.page.ts
+++ frontend/src/app/options/options.page.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonButton, ViewWillEnter, IonToggle } from '@ionic/angular/standalone';
+import { IonContent, IonButton, ViewWillEnter, IonToggle, AlertController } from '@ionic/angular/standalone';
 import { Router, ActivatedRoute } from '@angular/router';
 
 import { StorageService } from '../services/storage.service';
 import { DarkModeService } from '../services/dark-mode.service';
+import { MessageService } from '../services/message.service';
+import { ErrorService } from '../services/error.service';
+import { SharedVariablesService } from '../services/shared-variables.service';
 
 import { HeaderComponent } from '../header/header.component';
 import { NavbarComponent } from '../navbar/navbar.component';
@@ -24,13 +27,21 @@ export class OptionsPage implements ViewWillEnter {
   sectorId!: number;
   sector!: string;
 
+  mcqSize!: number;
+
   constructor(private router: Router,
               private route: ActivatedRoute,
               private storage: StorageService,
-              private darkmode: DarkModeService) { }
+              private darkmode: DarkModeService,
+              private alert: AlertController,
+              private message: MessageService,
+              private error: ErrorService,
+              private variables: SharedVariablesService) { }
 
   async ionViewWillEnter() {
     this.darkmode.init();
+
+    this.mcqSize = this.variables.mcqSize.large;
 
     const dark_mode_data = await this.storage.get("dark_mode_data");
     if(dark_mode_data != null) {
@@ -54,6 +65,66 @@ export class OptionsPage implements ViewWillEnter {
 
   toggleDarkPalette(shouldAdd: boolean) {
     document.documentElement.classList.toggle('ion-palette-dark', shouldAdd);
+  }
+
+  async clearProgress() {
+    const alert = await this.alert.create({
+      header: "Supprimer vos données pour cette filière ?",
+      message: "Cette action est irréversible.",
+      buttons: [
+        {
+          text: "Annuler",
+          role: "cancel",
+        },
+        {
+          text: "Supprimer",
+          handler: async () => {
+            this.message.sendMessage("getDefaultQuestionsData", {sectorId: this.sectorId, mcqSize: this.mcqSize}).subscribe(async res => {
+              console.log(res);
+              if(res.status == 200) {
+                let questions_data = await this.storage.get("questions_data");
+                questions_data[this.sectorId] = res.data[this.sectorId];
+                this.storage.set("questions_data", questions_data)
+              }
+              else {
+                this.error.errorMessage(res);
+              }
+            })
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+
+  async clearAllProgress() {
+    const alert = await this.alert.create({
+      header: "Supprimer toutes vos données ?",
+      message: "Cette action est irréversible.",
+      buttons: [
+        {
+          text: "Annuler",
+          role: "cancel",
+        },
+        {
+          text: "Supprimer",
+          handler: () => {
+            this.message.sendMessage("getDefaultQuestionsData", {sectorId: this.sectorId, mcqSize: this.mcqSize}).subscribe(res => {
+              console.log(res);
+              if(res.status == 200) {
+                this.storage.set("questions_data", res.data);
+              }
+              else {
+                this.error.errorMessage(res);
+              }
+            })
+          },
+        },
+      ],
+    });
+
+    await alert.present();
   }
 
   goToFAQ() {
