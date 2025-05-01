6d74456f1e1f92f5b33bc2d15ca358cc1c6fc340
Bectaly13 <dodocad13@gmail.com>
2025-05-01 23:53:24 +0200

    screen orientation

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { ScreenOrientation } from '@capacitor/screen-orientation';
+
+ScreenOrientation.lock({ orientation: 'portrait' });
 
 @Component({
   selector: 'app-root',
